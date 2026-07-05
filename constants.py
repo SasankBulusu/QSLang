1766bddd547116c0d58af5e21bcf3b5aad0ad27b
Sasank Bulusu <sasank.bulusu@gmail.com>
2026-07-05 17:26:00 +0530

    Add Hardcoded constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Hard-coded constraints for maximum allowed qubits and allowed gates 
+
+MAX_QUBITS = 5  
+
+# Every valid gate keyword the user can write
+ALLOWED_GATES = ["H", "X", "Y", "Z", "CNOT", "T", "S"]
+
+# The keyword to start a program
+INIT_KEYWORD = "INIT"
